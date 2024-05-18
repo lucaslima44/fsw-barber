e312e35d8ca0ac227cd38640cd523193b59ec381
lucaslima44 <lucaslima2011h@gmail.com>
2024-05-18 20:00:28 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Theo</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
