ea493b78fa9d6347d910387d020c8f9504a2b87d
lucaslima44 <lucaslima2011h@gmail.com>
2024-05-20 18:45:03 -0300

    fix: center footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="w-full bg-secondary px-5 py-6">
-      <p className="text-xs font-bold text-gray-400 opacity-75">
+      <p className="flex justify-center text-xs font-bold text-gray-400 opacity-75">
         © 2024 Copyright FSW Barber
       </p>
     </div>
