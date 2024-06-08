f95cdeb4b0b5c49461d8b43beb12c940c2e25330
lucaslima44 <lucaslima2011h@gmail.com>
2024-06-08 00:00:09 -0300

    feat: ensure booking can be canceled

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -45,9 +45,11 @@ const BookingsPage = async () => {
       <Header />
       <div className="px-5 py-6">
         <h1 className="text-xl font-bold">Agendamentos</h1>
-        <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
-          Confirmados
-        </h2>
+        {confirmedBookings.length === 0 && finishedBookings.length === 0 && (
+          <h2 className="mb-3 mt-6 text-sm font-bold uppercase text-gray-400">
+            Confirmados
+          </h2>
+        )}
         <div className="flex flex-col gap-3">
           {confirmedBookings.map((booking) => (
             <BookingItem key={booking.id} booking={booking} />
