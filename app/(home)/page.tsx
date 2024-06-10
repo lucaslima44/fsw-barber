f37cfc8251a25e08b1fd23f2973fbaf70c4db7e0
lucaslima44 <lucaslima2011h@gmail.com>
2024-06-09 21:31:16 -0300

    feat: add user name to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -34,7 +34,11 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Theo</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user
+            ? `Olá, ${session.user.name?.split(" ")[0]}!`
+            : "Olá!, vamos agendar um corte ?"}
+        </h2>
         <p className="text-sm capitalize">
           {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", {
             locale: ptBR,
